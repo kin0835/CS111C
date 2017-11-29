e2da038d436b3a38a9f4c80d56598e4f7b34dfb2
Wai Tam <tamwaikin926@gmail.com>
2017-11-29 01:47:12 -0800

    Update NoDupsPrioritizeArrayStack.java

--- LabE/Stack/NoDupsPrioritizeArrayStack.java
+++ LabE/Stack/NoDupsPrioritizeArrayStack.java
@@ -1,5 +1,6 @@
-
-public class NoDupsPrioritizeArrayStack<T> extends ArrayStack<T> implements NoDupsPrioritizeStackInterface<T> {
+@SuppressWarnings("unchecked")
+public class NoDupsPrioritizeArrayStack<T> extends ArrayStack<T>
+	implements NoDupsPrioritizeStackInterface<T> {
 	
 	public NoDupsPrioritizeArrayStack() {
 		super();
@@ -10,47 +11,38 @@ public class NoDupsPrioritizeArrayStack<T> extends ArrayStack<T> implements NoDu
 
 	@Override
 	public void push(T newEntry) {
-		if(contains(newEntry)){
-			super.push(newEntry);
-		}
+		if(indexLocate(newEntry) < 0)
+			super.push(newEntry);		
 	}
 
-	public void moveToTop(T entry) {		
-		if(contains(entry)){
-			int oldIndex = locateIndex(entry);
-			T oldEntry = stack[oldIndex];
-			removeGap(oldIndex);
-			createGap(0);
-			stack[0] = oldEntry;
-		}
+	public void moveToTop(T entry) {	
+		int index = indexLocate(entry);				
+		//not in stack
+		if(index < 0)
+			super.push(entry);
+		//in stack
 		else{
-			//add entry to top
+			T toTop = stack[index];
+			for(int i = index; i < topIndex; i++){
+				stack[i] = stack[i+1];
+			}
+			stack[topIndex] = toTop;	
 		}
 	} 
 
 	public void display() {
-		// YOUR CODE HERE
+		System.out.print("Display: \nBOTTOM ");		
+		for (int i = 0; i <= topIndex; i++)
+			System.out.print(stack[i] + " ");
+		System.out.println("TOP");
 	}
-	
-	private boolean contains (T entry){
-		if(locateIndex(entry) < 0)
-			return false;
-		else
-			return true;
-	}
-	
-	private int locateIndex (T entry){
-		for(int i = 0; i < topIndex; i++){
-			if(stack[i].equals(entry))
+
+	private int indexLocate (T entry){
+		for(int i = 0; i <= topIndex; i++){
+			if(stack[i].equals(entry)){
 				return i;
+			}
 		}
 		return -1;
-	}
-	
-	private void removeGap(int gapIndex){
-		
-	}
-	
-	private void createGap (int gapIndex){
 	}
 } 
